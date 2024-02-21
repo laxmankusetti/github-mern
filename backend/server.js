1c6e1ab53997d4c5d1dc72f3b2be61a73708417d
laxmankusetti <laxmankusetti@gmail.com>
2024-02-21 18:08:39 +0530

    package and server.js files updted and optimized for build

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import passport from 'passport';
 import session from 'express-session';
+import path from 'path';
 
 import userRouter from './roures/user.routes.js';
 import exploreRouter from './roures/explore.routes.js';
@@ -15,7 +16,11 @@ import { connectMongoDB } from './db/connectMongoDb.js';
 
 dotenv.config();
 
+const port = process.env.PORT || 5000;
+
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -31,7 +36,13 @@ app.use('/api/user', userRouter);
 app.use('/api/explore', exploreRouter);
 app.use('/api/auth', authRouter);
 
-app.listen(5000, () => {
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
+app.listen(port, () => {
     connectMongoDB();
     console.log('App is running on the port : 5000')
 })
